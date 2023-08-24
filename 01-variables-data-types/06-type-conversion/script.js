1b03070db5de011d5e23c1f3c8807427822f8c80
Sohag Himon <sohagdev19@gmail.com>
2023-08-24 22:40:07 +0600

    worked on type-conversion

---
+++ 01-variables-data-types/06-type-conversion/script.js
@@ -0,0 +1,19 @@
+let amount = 'Hello'
+
+// Convert String to Number
+amount = parseInt(amount)
+// amount = +amount
+// amount = Number(amount)
+
+// Convert number to string
+// amount = amount.toString()
+// amount = String(amount)
+
+// Convert string to decimal
+// amount = parseFloat(amount)
+
+// Convert NUmber to Boolean
+// amount = Boolean(amount)
+
+console.log(Math.sqrt(-1))
+console.log(amount, typeof amount)

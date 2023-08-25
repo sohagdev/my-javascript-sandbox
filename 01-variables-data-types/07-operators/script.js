eb7607f0437e89fd1ac1766372f4c54aabb73607
Sohag Himon <sohagdev19@gmail.com>
2023-08-25 23:18:30 +0600

    Learned about the oparetors

--- 01-variables-data-types/07-operators/script.js
+++ 01-variables-data-types/07-operators/script.js
@@ -1,7 +1,27 @@
 // 1. Arithmetic Operators
 
+let x
 
+x = 5 + 5
+x = 5 - 5
+x = 5 * 5
+x = 5 / 5
+x = 7 % 5
+
+// Concatenation
+x = 'Hello' + ' ' + 'World'
+
+// Exponent
+x = 2 ** 3
+
+// Increment
+x = 1
+x = x + 1
+x++
+// Decrement
+x--
+
+console.log(x)
 // 2. Assignment Operators
-
 
 // 3. Comparison Operators

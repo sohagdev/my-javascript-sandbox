e06abac55ba93b1de53574b4bb1598fa7e94c6fe
Sohag Himon <sohagdev19@gmail.com>
2023-08-26 22:47:53 +0600

    learned type coercion

---
+++ 01-variables-data-types/08-type-coercion/script.js
@@ -0,0 +1,18 @@
+let x
+
+x = 5 + '5'
+
+x = 5 + Number('5')
+
+x = 5 * '5'
+
+x = 5 + null
+
+x = Number(null)
+x = Number(true)
+x = Number(false)
+
+x = 5 + true
+x = 5 + false
+
+console.log(x, typeof x)
